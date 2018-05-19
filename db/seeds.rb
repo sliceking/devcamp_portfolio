abf2d1903a5aa423360512f820f22fe6948617c4
stan wielga <svwielga4@gmail.com>
2018-05-19 12:49:50 -0700

    created seeds file for sample data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,29 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+10.times do |blog|
+    Blog.create!(
+        title:"my blog post #{blog}",
+        body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus id ullamcorper turpis. Duis tortor nisi, venenatis ultrices fringilla eu, dictum mollis orci. Nulla accumsan felis orci, nec eleifend dui elementum sed. Morbi tempor elit justo, id sagittis neque molestie ultrices. Cras vel velit malesuada, sodales sem ac, ullamcorper tortor. Etiam congue tortor eu nisi fringilla lacinia. Nunc at rutrum turpis. Aliquam erat volutpat. Quisque sed interdum quam. Fusce vitae neque tincidunt, placerat leo sit amet, maximus sapien. Cras ligula lorem, tristique in semper vitae, ullamcorper et turpis.'
+    )
+end
+
+puts "10 blogs created"
+
+5.times do |skill|
+    Skill.create!(
+        title: "Rails #{skill}",
+        percent_utilized: 15
+    )
+end
+
+puts "5 skills created"
+
+9.times do |portfolio_item|
+    Portfolio.create!(
+        title: "Portfolio title: #{portfolio_item}",
+        subtitle: "My great service",
+        body: "Nunc non gravida tellus. Sed eget mollis leo. Aliquam mauris enim, convallis vitae dui nec, blandit faucibus enim. Nunc tristique bibendum felis sagittis ultricies. Etiam vitae interdum tortor. Cras eget feugiat odio. Ut eget placerat elit, id tempor magna. Nullam quis lobortis ante. Morbi accumsan risus sit amet erat luctus, id iaculis ex fringilla. ",
+        main_image: "http://via.placeholder.com/600x400",
+        thumb_image: "http://via.placeholder.com/350x200"
+    )
+end
+
+puts "9 portfolio items created"
