2d760fcf00c48400e128753264abb8a7375c48d4
stan wielga <svwielga4@gmail.com>
2018-05-19 23:45:01 -0700

    added show to portfolio module

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -36,4 +36,11 @@ class PortfoliosController < ApplicationController
           end
         end
     end
+
+    def show 
+        @portfolio_item = Portfolio.find(params[:id])
+
+        
+
+    end
 end
