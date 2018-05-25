ced9535242ceb1280a2c8db5c1c99beb56ae7850
stan wielga <svwielga4@gmail.com>
2018-05-25 15:05:39 -0700

    integrated new gem into app for copyright

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,10 @@ module ApplicationHelper
             content_tag(:p, greeting, class: "source-greeting")
         end
     end
+
+
+
+    def copyright_generator
+        @copyright = WielgaDevcampViewTool::Renderer.copyright "Stan Wielga", "All rights reserved"
+    end
 end
