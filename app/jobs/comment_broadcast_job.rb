691d160c8b250bdebc65975596de6b0c7a387584
svwielga4@gmail.com <svwielga4@gmail.com>
2018-05-29 10:18:48 -0700

    implemented action cable for comments

--- app/jobs/comment_broadcast_job.rb
+++ app/jobs/comment_broadcast_job.rb
@@ -2,7 +2,7 @@ class CommentBroadcastJob < ApplicationJob
   queue_as :default
 
   def perform(comment)
-    ActionCable.server.broadcast "blogs_#{comment.blog.id}_channel", comment: render_comment()
+    ActionCable.server.broadcast "blogs_#{comment.blog.id}_channel", comment: render_comment(comment)
   end
 
   private
